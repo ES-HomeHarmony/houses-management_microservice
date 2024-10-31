ae22993034e86e8a8096404bd7d754e7b588a385
Tiago Gomes <tiagocgomes@ua.pt>
2024-10-31 23:00:21 +0000

    SCRUM-95 API development for houses creation done

--- app/models.py
+++ app/models.py
@@ -1,15 +1,31 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Double
 from sqlalchemy.orm import relationship
-from .database import Base
+from app.database import Base
 
 class House(Base):
     __tablename__ = 'houses'
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True)
-    tenants_count = Column(Integer)  # Número de inquilinos
-    # landloard_id = Column(str(255)) 
+    landlord_id = Column(String(255))  # Relação com a tabela 'users' id do cognito
+    address = Column(String(255))
+    city = Column(String(255))
+    state = Column(String(255))
+    zipcode = Column(String(255))
+
+    # tenents = relationship("Tenents", back_populates="house")  # Relação com a tabela 'tenents'
 
     # Comentar landlord_id temporariamente
     # landlord_id = Column(Integer, ForeignKey('users.id'))
     # landlord = relationship("User")  # Relação com a tabela 'users'
+
+# class Tenents(Base):
+#     __tablename__ = 'tenents'
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     house_id = Column(Integer, ForeignKey('houses.id')) # Relação com a tabela 'houses'
+#     rent = Column(Double)
+#     tenent_id = Column(String(255))  # Relação com a tabela 'users' id do cognito
+
+#     house = relationship("House", back_populates="tenents")  # Relação com a tabela 'houses'
+
