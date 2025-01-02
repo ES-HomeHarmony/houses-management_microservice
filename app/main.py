d26a2130c3c0573dc415e1e4232b17dde4fe05ed
Roberto <robertorcastro@ua.pt>
2025-01-02 19:57:47 +0000

    SCRUM-130 | Tests Done and passing

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import landlords_routes, tenants_routes
 from app.database import Base, engine
-import os
 
 app = FastAPI()
 
@@ -14,12 +13,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 # Cria todas as tabelas no DB
 Base.metadata.create_all(bind=engine)
 
 # Incluir os endpoints relacionados a expenses
 app.include_router(landlords_routes.router)
 app.include_router(tenants_routes.router)
-
-
